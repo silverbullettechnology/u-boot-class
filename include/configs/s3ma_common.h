fc0aa17ef8fef01c29d47af40bc2f8b37545f51b
dmitriygoldyuk <dmitriy.goldyuk@exelisinc.com>
2014-06-10 14:00:34 -0500

    inital configuration for s3ma asic and development board

---
+++ include/configs/s3ma_common.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __S3MA_COMMON_H
+#define __S3MA_COMMON_H
+
+#define CONFIG_ARM_ERRATA_742230
+#define CONFIG_ARM_ERRATA_743622
+#define CONFIG_ARM_ERRATA_751472
+#define CONFIG_ARM_ERRATA_794072
+#define CONFIG_ARM_ERRATA_761320
+#define CONFIG_BOARD_POSTCLK_INIT
+
+#ifndef CONFIG_SYS_L2CACHE_OFF
+#define CONFIG_SYS_L2_PL310
+
+#if 0
+#define CONFIG_SYS_PL310_BASE	L2_PL310_BASE
+#else
+#define CONFIG_SYS_PL310_BASE	S3MA_L2_PL310_BASE //Needs to be defined in s3ma_regs.h
+#endif
+
+#endif
+
+#endif
