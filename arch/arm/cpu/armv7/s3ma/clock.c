3a5e172b8f86ba529a10c10a5d7a6eb9aa62b765
dmitriygoldyuk <dmitriy.goldyuk@exelisinc.com>
2014-06-19 17:25:41 -0500

    replacing register bit access with macros

--- arch/arm/cpu/armv7/s3ma/clock.c
+++ arch/arm/cpu/armv7/s3ma/clock.c
@@ -23,42 +23,22 @@ void s3ma_setup_pll(void)
 
 void s3ma_ddr_clock_enable(void)
 {
-	u32 mru_clk_dis_reg;
-
-	mru_clk_dis_reg = readl(CRT_CLK_DIS);
-
-	mru_clk_dis_reg &= ~DMC_DIS_BITMASK;
-
-	mru_clk_dis_reg |= ((u32)DMC_DIS_RESET << DMC_DIS_SHIFT) & (u32)DMC_DIS_BITMASK;
-
-	writel(mru_clk_dis_reg, CRT_CLK_DIS);
+	clrbits_le32(CRT_CLK_DIS, DMC_DIS_BITMASK);
 }
 
 void s3ma_ddr_phy_clk_sel_384(void)
 {
-	u32 reg;
-
 	//mon_puts("BEGIN CHANGE FREQUENCY OF DDR I/F: 192->384\n");
 
 	// mon_puts("Disable clock to DDR I/F\n");
-	reg = readl(CRT_CLK_DIS);
-	reg |= (u32)0x0 |((u32)0x1 << (DMC_DIS_SHIFT + 2));// "ddr_phy_clk"
-	writel(reg,CRT_CLK_DIS);
-
-	//REG32(DDR_CFG);					// use as delay
+	setbits_le32(CRT_CLK_DIS, (uint32_t)0x1 << (DMC_DIS_SHIFT + 2));// "ddr_phy_clk"
 	__udelay(1);
 
 	// mon_puts("Select 384MHz DDR I/F clock\n");
-	reg = readl(DDR_CFG);
-	reg |= (u32)0x0 | ((u32)0x1 << DDR_PHY_CLK_SEL_SHIFT);
-	writel(reg,DDR_CFG);		// use "clk_384"
+	setbits_le32(DDR_CFG, (uint32_t)0x1 << DDR_PHY_CLK_SEL_SHIFT);// use "clk_384"
+	__udelay(1);
 
-	// mon_puts("Enable clock to DDR I/F\n");
-	reg = readl(CRT_CLK_DIS);
-	reg &= ~((u32)0x0 |((u32)0x1 << (DMC_DIS_SHIFT + 2)));// "ddr_phy_clk"
-
-	writel(reg,CRT_CLK_DIS);	// "ddr_phy_clk"
-	// REG32(DDR_CFG);					// use as delay
+	clrbits_le32(CRT_CLK_DIS, (uint32_t)0x1 << (DMC_DIS_SHIFT + 2));// "ddr_phy_clk"
 	__udelay(1);
 
 	// mon_puts("  END CHANGE FREQUENCY OF DDR I/F: 192->384\n");
@@ -66,27 +46,16 @@ void s3ma_ddr_phy_clk_sel_384(void)
 
 void s3ma_ddr_phy_clk_sel_192(void)
 {
-	u32 reg;
-	// mon_puts("BEGIN CHANGE FREQUENCY OF DDR I/F: 384->192\n");
 
-	// mon_puts("Disable clock to DDR I/F\n");
-	reg = readl(CRT_CLK_DIS);
-	reg |= (u32)0x0 |((u32)0x1 << (DMC_DIS_SHIFT + 2));// "ddr_phy_clk"
-	writel(reg,CRT_CLK_DIS);
-	// REG32(DDR_CFG);					// use as delay
+	setbits_le32(CRT_CLK_DIS,(uint32_t)0x1 << (DMC_DIS_SHIFT + 2)); // "ddr_phy_clk"
 	__udelay(1);
 
 	// mon_puts("Select 192MHz DDR I/F clock\n");
-	reg = readl(DDR_CFG);
-	reg &= ~((u32)0x0 | ((u32)0x1 << DDR_PHY_CLK_SEL_SHIFT));
-	writel(reg,DDR_CFG);// use "clk_192"
+	clrbits_le32(DDR_CFG,(uint32_t)0x1 << DDR_PHY_CLK_SEL_SHIFT); // use "clk_192"
 	__udelay(1);
 
 	// mon_puts("Enable clock to DDR I/F\n");
-	reg = readl(CRT_CLK_DIS);
-	reg &= ~((u32)0x0 |((u32)0x1 << (DMC_DIS_SHIFT + 2)));// "ddr_phy_clk"
-
-	writel(reg,CRT_CLK_DIS);	// "ddr_phy_clk"
+	clrbits_le32(CRT_CLK_DIS, (uint32_t)0x1 << (DMC_DIS_SHIFT + 2)); // "ddr_phy_clk"
 	__udelay(1);
 
 //	mon_puts("  END CHANGE FREQUENCY OF DDR I/F: 384->192\n");
