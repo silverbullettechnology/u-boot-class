f0e627f5eeaf5b8e316c369d14f5208ec52e52fa
dmitriygoldyuk <dmitriy.goldyuk@exelisinc.com>
2014-07-28 09:41:11 -0500

    Adding USB controller driver

---
+++ arch/arm/include/asm/arch-s3ma/hardware.h
@@ -0,0 +1,17 @@
+/*
+ * hardware.h
+ *
+ *  Created on: Jul 25, 2014
+ *      Author: user
+ */
+
+#ifndef _ASM_ARCH_HARDWARE_H
+#define _ASM_ARCH_HARDWARE_H
+
+#define CONFIG_SYS_USBD_BASE	(USB0_S_DCFG)
+#define CONFIG_SYS_PLUG_BASE	(USB0_S_DCFG)
+#define CONFIG_SYS_FIFO_BASE	(USB0_S_DCFG)
+
+
+
+#endif /*_ASM_ARCH_HARDWARE_H */
