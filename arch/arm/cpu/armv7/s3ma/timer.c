ab436e2d059e2cbf8ac6c576a7aed6cbbe9b711d
dmitriygoldyuk <dmitriy.goldyuk@exelisinc.com>
2015-04-22 15:34:04 -0500

    TIMER: Switching from General shared time to A9 private timer

--- arch/arm/cpu/armv7/s3ma/timer.c
+++ arch/arm/cpu/armv7/s3ma/timer.c
@@ -12,6 +12,142 @@
 #include <div64.h>
 #include <asm/arch/clock.h>
 
+#if 1
+DECLARE_GLOBAL_DATA_PTR;
+
+struct scu_timer {
+	u32 load; /* Timer Load Register */
+	u32 counter; /* Timer Counter Register */
+	u32 control; /* Timer Control Register */
+};
+
+static struct scu_timer *timer_base =
+			      (struct scu_timer *)(PTIMER0_ABSOLUTE_BASE);
+
+#define SCUTIMER_CONTROL_PRESCALER_MASK	0x0000FF00 /* Prescaler */
+#define SCUTIMER_CONTROL_PRESCALER_SHIFT	8
+#define SCUTIMER_CONTROL_AUTO_RELOAD_MASK	0x00000002 /* Auto-reload */
+#define SCUTIMER_CONTROL_ENABLE_MASK		0x00000001 /* Timer enable */
+
+#define TIMER_LOAD_VAL 0xFFFFFFFF
+#define TIMER_PRESCALE 255
+
+int timer_init(void)
+{
+	const u32 emask = SCUTIMER_CONTROL_AUTO_RELOAD_MASK |
+			(TIMER_PRESCALE << SCUTIMER_CONTROL_PRESCALER_SHIFT) |
+			SCUTIMER_CONTROL_ENABLE_MASK;
+
+	gd->arch.timer_rate_hz = (gd->cpu_clk / 2) / (TIMER_PRESCALE + 1);
+
+	/* Load the timer counter register */
+	writel(0xFFFFFFFF, &timer_base->load);
+
+	/*
+	 * Start the A9Timer device
+	 * Enable Auto reload mode, Clear prescaler control bits
+	 * Set prescaler value, Enable the decrementer
+	 */
+	clrsetbits_le32(&timer_base->control, SCUTIMER_CONTROL_PRESCALER_MASK,
+								emask);
+
+	/* Reset time */
+	gd->arch.lastinc = readl(&timer_base->counter) /
+				(gd->arch.timer_rate_hz / CONFIG_SYS_HZ);
+	gd->arch.tbl = 0;
+
+	return 0;
+}
+
+/*
+ * This function is derived from PowerPC code (read timebase as long long).
+ * On ARM it just returns the timer value.
+ */
+ulong get_timer_masked(void)
+{
+	ulong now;
+
+	now = readl(&timer_base->counter) /
+			(gd->arch.timer_rate_hz / CONFIG_SYS_HZ);
+
+	if (gd->arch.lastinc >= now) {
+		/* Normal mode */
+		gd->arch.tbl += gd->arch.lastinc - now;
+	} else {
+		/* We have an overflow ... */
+		gd->arch.tbl += gd->arch.lastinc + TIMER_LOAD_VAL - now + 1;
+	}
+	gd->arch.lastinc = now;
+
+	return gd->arch.tbl;
+}
+
+void __udelay(unsigned long usec)
+{
+	u32 countticks;
+	u32 timeend;
+	u32 timediff;
+	u32 timenow;
+
+	if (usec == 0)
+		return;
+
+	countticks = lldiv(((unsigned long long)gd->arch.timer_rate_hz * usec),
+			   1000000);
+
+	/* decrementing timer */
+	timeend = readl(&timer_base->counter) - countticks;
+
+#if TIMER_LOAD_VAL != 0xFFFFFFFF
+	/* do not manage multiple overflow */
+	if (countticks >= TIMER_LOAD_VAL)
+		countticks = TIMER_LOAD_VAL - 1;
+#endif
+
+	do {
+		timenow = readl(&timer_base->counter);
+
+		if (timenow >= timeend) {
+			/* normal case */
+			timediff = timenow - timeend;
+		} else {
+			if ((TIMER_LOAD_VAL - timeend + timenow) <=
+								countticks) {
+				/* overflow */
+				timediff = TIMER_LOAD_VAL - timeend + timenow;
+			} else {
+				/* missed the exact match */
+				break;
+			}
+		}
+	} while (timediff > 0);
+}
+
+/* Timer without interrupts */
+ulong get_timer(ulong base)
+{
+	return get_timer_masked() - base;
+}
+
+/*
+ * This function is derived from PowerPC code (read timebase as long long).
+ * On ARM it just returns the timer value.
+ */
+unsigned long long get_ticks(void)
+{
+	return get_timer(0);
+}
+
+/*
+ * This function is derived from PowerPC code (timebase clock frequency).
+ * On ARM it returns the number of timer ticks per second.
+ */
+ulong get_tbclk(void)
+{
+	return CONFIG_SYS_HZ;
+}
+
+#else
 /* MPCore Global timer registers */
 typedef struct globaltimer {
 	u32 cnt_l; /* 0x00 */
@@ -30,19 +166,11 @@ DECLARE_GLOBAL_DATA_PTR;
 
 static inline unsigned long long tick_to_time(unsigned long long tick)
 {
-#if 0
-	tick *= CONFIG_SYS_HZ;
-	do_div(tick, MXC_CLK32);
-#endif
-	return tick;
+	return CONFIG_SYS_HZ;
 }
 
 static inline unsigned long long us_to_tick(unsigned long long usec)
 {
-#if 0
-	usec = usec * MXC_CLK32 + 999999;
-	do_div(usec, 1000000);
-#endif
 	return usec;
 }
 
@@ -97,7 +225,7 @@ int timer_init(void)
 	writel(GT_CNTL_REG_RESET, &gt->ctl);
 	/* Make timer tick at 1000*CONFIG_SYS_HZ rate */
 	clrbits_le32(&gt->ctl,PRESCALER_BITMASK);
-	setbits_le32(&gt->ctl,(lldiv((uint64_t)gd->bus_clk, (uint32_t)1000*CONFIG_SYS_HZ) - 1)<< PRESCALER_SHIFT);
+	setbits_le32(&gt->ctl,(lldiv((uint64_t)gd->cpu_clk/2, (uint32_t)1000*CONFIG_SYS_HZ) - 1)<< PRESCALER_SHIFT);
 	/* Disable interrupts */
 	clrbits_le32(&gt->ctl, IRQ_EN_BITMASK);
 	/* Clear counter value */
@@ -180,3 +308,4 @@ ulong get_tbclk(void)
 #endif
 	return 1000;
 }
+#endif
